e738ab1a9810b260b763d806144ae901c12fed57
Todd <creaux@gmail.com>
2015-05-03 20:32:31 -0400

    Testing new array system

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-var list = [1,2]; //Holds the list array
-var selection;
-var remove;
-var entryText;
 
-
-    function addEntry() {
-        var entry = document.getElementById('entryBox');
-        list = list.concat(entry.value);
-		alert(list);
-		console.log(list);
-    }
-	
-/*	
-function addEntry(){ //adds text to $list array
-	var entryBox=document.getElementById("entryBox").value;
-	entryBox.textContent=entryText;
-	list = list.concat(entryText);
-	alert(list);
-	
-};
-*/
+   var list=["1"]
+   function addEntry()
+   {
+    var output=document.getElementById("outputList");
+     list.push(document.getElementById("entryBox").value);
+    output.innerHTML=list.join('<br/>'); 
+   }
